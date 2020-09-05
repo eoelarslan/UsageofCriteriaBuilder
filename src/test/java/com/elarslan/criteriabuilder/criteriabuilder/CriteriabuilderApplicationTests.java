20065f8f62c656f588465383588c9ef7b5393af7
eoelarslan <ersin.elarslan@gmail.com>
2020-09-05 15:52:42 +0300

    Initial commit

---
+++ src/test/java/com/elarslan/criteriabuilder/criteriabuilder/CriteriabuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elarslan.criteriabuilder.criteriabuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriabuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
